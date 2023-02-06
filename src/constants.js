e3f10f84646b53e3962451cbeab275f03eea9853
Trunova Yulia <trunovaf11@gmail.com>
2023-02-06 18:52:05 +0100

    js-10

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const DEBOUNCE_DELAY = 600;
+export const COUNTRIES_API = txt =>
+  `https://restcountries.com/v3.1/name/${txt}`;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import './css/styles.css';
+import _debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { DEBOUNCE_DELAY, COUNTRIES_API } from './constants.js';
 
-const DEBOUNCE_DELAY = 300;
+const countryList = document.getElementById('country-list');
+
+const fetchCountries = txt => {
+  if (!txt) {
+    countryList.innerHTML = '';
+    return;
+  }
+  fetch(COUNTRIES_API(txt))
+    .then(response => {
+      if (!response.ok) {
+        countryList.innerHTML = `<h2>No country with that name</h2>`;
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      } else return response.json();
+    })
+    .then(countries => {
+      if (countries.length > 1 && countries.length < 11) {
+        const countriesArray = countries
+          .map(
+            item =>
+              `<li><img style="width: 30px" src=${item.flags.svg} />${item.name.common}</li>`
+          )
+          .join('');
+        countryList.innerHTML = countriesArray;
+      } else if (countries.length === 1)
+        countryList.innerHTML = `<h2>
+        <img style="width: 60px" src=${countries[0].flags.svg} />
+        ${countries[0].name.common}</h2>
+      <p><b>Capital:</b> ${countries[0].capital}</p>
+      <p><b>Population:</b> ${countries[0].population}</p>
+      <p><b>Languages:</b> ${Object.values(countries[0].languages).join(
+        ', '
+      )}</p>
+      `;
+      else if (countries.length > 10) {
+        countryList.innerHTML = ``;
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
+      } else if (countries.length === 0)
+        countryList.innerHTML = `<h2>Нет результатов</h2>`;
+    })
+    .catch(err => console.log('error'));
+};
+
+const fetchCountriesDebounced = _debounce(fetchCountries, DEBOUNCE_DELAY);
+
+const searchBox = document.getElementById('search-box');
+searchBox.addEventListener('input', function (e) {
+  fetchCountriesDebounced(e.target.value);
+});
